69e703bd09432771c1eb4867ff9fb059923673f9
Aria Andika <andikaakun17@gmail.com>
2024-09-30 15:00:30 +0700

    deez

---
+++ bin/karyawan.dart
@@ -0,0 +1,10 @@
+import 'package:karyawan/karyawan.dart';
+
+void main(List<String> arguments) {
+  List<Karyawan> dataKaryawan = genData(dummyData());
+
+  for (var staff in dataKaryawan) {
+    print(staff.deskripsi());
+  }
+
+}
